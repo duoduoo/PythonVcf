69961f40d012e639356730a102eb62500ac6b993
Duo 'Erica' Xu <xdd.erica@gmail.com>
2016-03-10 18:03:27 -0500

    Added files via upload
    # get vcf file for a specific region.
    # inputs: vcffile.vcf #chr start end

---
+++ extract_vcf.py
@@ -0,0 +1,29 @@
+# get vcf file for a specific region.
+# inputs: vcffile.vcf #chr start end
+
+import sys
+import re
+import string
+
+if len(sys.argv) != 6:
+    print "Usage: python extract_vcf.py [inputvcf] [#chr] [start] [end] [output]"
+    sys.exit (1)
+    
+input = open(sys.argv[1], 'r')
+chr = int(sys.argv[2])
+start = int(sys.argv[3])
+end = int(sys.argv[4])
+output = open(sys.argv[5], 'w')
+
+for l in input:
+    l = l.strip()
+    if l.startswith('#'):
+        output.write(l+'\n')
+        continue
+    fields = l.split('\t')
+    if fields[0] == 'chr'+str(chr):
+        if int(fields[1]) >= start:
+            if int(fields[1]) <= end:
+                output.write(l+'\n')
+                
+output.close()
